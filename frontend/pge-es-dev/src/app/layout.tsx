3992377621f0b2b94a8d66a1512f5a1b538c7ec7
matheus-nantes <matheus.nantes246@gmail.com>
2024-07-30 03:19:15 -0400

    feat: criando estrutura inicial do frontend. closes #11

---
+++ frontend/pge-es-dev/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "PGE-ES-DEV",
+  description: "Sistema desenvolvido conforme o solicitado no desafio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-white">{children}</body>
+    </html>
+  );
+}
---
+++ frontend/pge-es-dev/src/app/processos/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main>
+        <h1>Processos</h1>
+      </main>
+    );
+  }
+  
